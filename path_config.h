18e7ef6ad69d1818f21b7c3d5494fb6842d1f7b2
yazhongyang <yazhongyang@cmail.carleton.ca>
2022-12-09 22:31:16 -0500

    1

--- path_config.h
+++ path_config.h
@@ -1,3 +1,3 @@
-#define MATERIAL_DIRECTORY "D:\\SchoolProject\\COMP\\3501\\Project-master\\shader"
-#define TEXTURE_DIRECTORY "D:\\SchoolProject\\COMP\\3501\\Project-master\\texture"
+#define MATERIAL_DIRECTORY "D:\\2022fall\\comp3501\\project\\final\\shader"
+#define TEXTURE_DIRECTORY "D:\\2022fall\\comp3501\\project\\final\\texture"
 // change to specify your own location here
